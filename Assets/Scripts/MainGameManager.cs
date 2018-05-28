259ed0692a1a117984d80b3a0be871915de9a894
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-05-28 19:49:43 +0800

    MainGameManger 的创建

---
+++ Assets/Scripts/MainGameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGameManager : MonoBehaviour
+{
+    private static MainGameManager _instance;
+
+    public static MainGameManager Instance { get { return _instance; } }
+
+    [Header("元素预制体"),SerializeField]
+    private GameObject bgElementPrefab;
+    [Tooltip("边界预制体,顺序为:"), SerializeField,/*Space(50)*/]
+    private GameObject[] borderElementsPrefabs;
+
+    [Header("地图设置")]
+    [SerializeField]
+    private int w;
+    [SerializeField]
+    private int  h;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+}
