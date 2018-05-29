18e951ec45609444d4dc06accf85c336538701cb
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-05-29 16:48:37 +0800

    地图和摄像机的制作   基类的制作

---
+++ Assets/Scripts/BaseElement.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ElementState
+{
+    Covered,
+    UnCovered,
+    Marked
+}
+
+public enum ElementType
+{
+    SingleCovered,
+    DoubleCovered,
+    CantCovered,
+}
+
+public enum ElementContent
+{
+    Number,
+    Trap,
+    Tool,
+    Gold,
+    Enemy,
+    Door
+}
+
+public class BaseElement : MonoBehaviour
+{
+    private int x, y;
+    private ElementState elementState;
+    private ElementType elementType;
+    private ElementContent elementContent;
+
+    public virtual void Awake()
+    {
+        x = (int)transform.position.x;
+        y = (int)transform.position.y;
+        name = "(" + x + "," + y + ")";
+    }
+
+    public void LoadSprite(Sprite sprite)
+    {
+        gameObject.GetComponent<SpriteRenderer>()
+            .sprite = sprite;
+    }
+
+    public virtual void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(2) && elementState == ElementState.UnCovered)
+        {
+            OnMiddleMouseButton();
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            OnLeftMouseButton();
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            OnRightMouseButton();
+        }
+    }
+
+    public virtual void OnLeftMouseButton()
+    {
+
+    }
+
+    public virtual void OnMiddleMouseButton()
+    {
+
+    }
+
+
+    public virtual void OnRightMouseButton()
+    {
+
+    }
+
+    public virtual void OnPlayerStand()
+    {
+
+    }
+}
--- Assets/Scripts/MainGameManager.cs
+++ Assets/Scripts/MainGameManager.cs
@@ -4,23 +4,76 @@ using UnityEngine;
 
 public class MainGameManager : MonoBehaviour
 {
-    private static MainGameManager _instance;
+    public static MainGameManager Instance { get; private set; }
 
-    public static MainGameManager Instance { get { return _instance; } }
-
-    [Header("元素预制体"),SerializeField]
+    [Header("背景预制体"), SerializeField]
     private GameObject bgElementPrefab;
-    [Tooltip("边界预制体,顺序为:"), SerializeField,/*Space(50)*/]
+    [Tooltip("边界预制体,顺序为:\n上,下,左,右,左上,右上,左下,右下")
+        , SerializeField,/*Space(50)*/]
     private GameObject[] borderElementsPrefabs;
+    [Header("泥土预制体"), SerializeField]
+    private BaseElement baseElement;
 
-    [Header("地图设置")]
-    [SerializeField]
+
+
+    [Header("关卡设置"), SerializeField]
     private int w;
     [SerializeField]
-    private int  h;
+    private int h;
+
+    private BaseElement[,] mapArray;
 
     private void Awake()
     {
-        _instance = this;
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        CreateMap();
+        ResetCamera();
+    }
+
+    private void CreateMap()
+    {
+        var holder = new GameObject("Element_Holder").transform;
+        var bgParent = new GameObject("Backgrounds").transform;
+        bgParent.SetParent(holder);
+        var coverParent = new GameObject("Covers").transform;
+        coverParent.SetParent(holder);
+
+        for (int i = 0; i < w; i++)
+        {
+            for (int j = 0; j < h; j++)
+            {
+                Instantiate(bgElementPrefab, new Vector3(i, j, 0)
+                    , Quaternion.identity, bgParent);
+                mapArray[i, j] = Instantiate(baseElement, new Vector3(i, j, 0)
+                    , Quaternion.identity, coverParent);
+            }
+        }
+
+        for (int i = 0; i < w; i++)
+        {
+            Instantiate(borderElementsPrefabs[0], new Vector3(i, h + 0.25f, 0), Quaternion.identity, bgParent);
+            Instantiate(borderElementsPrefabs[1], new Vector3(i, -1.25f, 0), Quaternion.identity, bgParent);
+        }
+
+        for (int i = 0; i < h; i++)
+        {
+            Instantiate(borderElementsPrefabs[2], new Vector3(-1.25f, i, 0), Quaternion.identity, bgParent);
+            Instantiate(borderElementsPrefabs[3], new Vector3(w + 0.25f, i, 0), Quaternion.identity, bgParent);
+        }
+        Instantiate(borderElementsPrefabs[4], new Vector3(-1.25f, h + 0.25f, 0), Quaternion.identity, bgParent);
+        Instantiate(borderElementsPrefabs[5], new Vector3(w + 0.25f, h + 0.25f, 0), Quaternion.identity, bgParent);
+        Instantiate(borderElementsPrefabs[6], new Vector3(-1.25f, -1.25f, 0), Quaternion.identity, bgParent);
+        Instantiate(borderElementsPrefabs[7], new Vector3(w + 0.25f, -1.25f, 0), Quaternion.identity, bgParent);
+    }
+
+    private void ResetCamera()
+    {
+        Camera camera = Camera.main;
+        Camera.main.orthographicSize = (h + 3) / 2f;
+        camera.transform.position = new Vector3((w - 1) / 2f, (h - 1) / 2f, -10);
     }
 }
