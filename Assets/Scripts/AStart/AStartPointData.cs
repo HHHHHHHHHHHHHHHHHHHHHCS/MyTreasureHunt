7f0c3080678bd883205a311d00e5ef67970acaf7
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-06-08 18:11:49 +0800

    制作A* 算法未完成

---
+++ Assets/Scripts/AStart/AStartPointData.cs
@@ -0,0 +1,36 @@
+﻿public struct AStartPoint
+{
+    public int x, y;
+
+    public AStartPoint(int _x, int _y)
+    {
+        x = _x;
+        y = _y;
+    }
+
+    public bool Equals(AStartPoint obj)
+    {
+        return x == obj.x && y == obj.y;
+    }
+}
+
+public class AStartPointData
+{
+    public AStartPoint pointPos;
+    public double g, h;
+    public AStartPointData parent;
+
+    public double F { get { return g + h; } }
+
+
+    public AStartPointData(AStartPoint _AStartPoint, double _g, double _h, AStartPointData _parent)
+    {
+        pointPos = _AStartPoint;
+        g = _g;
+        h = _h;
+        parent = _parent;
+    }
+
+
+}
+
