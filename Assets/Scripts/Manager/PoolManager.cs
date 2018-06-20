30df250bc74ecd2bc8cdf9a6bf9cf001a33bdc11
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-06-20 11:10:38 +0800

    资源池框架的完成

--- Assets/Scripts/Manager/PoolManager.cs
+++ Assets/Scripts/Manager/PoolManager.cs
@@ -7,18 +7,17 @@ public enum EffectType
     SmokeEffect,
     UnCoveredEffect,
     GoldEffect,
-    DoorOopenEffect
-
 }
 
 
-public sealed class PoolManager 
+public sealed class PoolManager
 {
     private static PoolManager _instance;
-    public static PoolManager Instance {
+    public static PoolManager Instance
+    {
         get
         {
-            if(_instance==null)
+            if (_instance == null)
             {
                 _instance = new PoolManager().OnInit();
             }
@@ -27,8 +26,8 @@ public sealed class PoolManager
     }
 
     private Dictionary<EffectType, GameObject> effectGOPrefabDic;
-    private Dictionary<EffectType, List<GameObject>> effectPoolDic ;
-    private List<GameObject> smokeEffect, unCoveredEffect, goldEffect, doorOpenEffect;
+    private Dictionary<EffectType, List<GameObject>> effectPoolDic;
+    //private List<GameObject> smokeEffect, unCoveredEffect, goldEffectx;
     private int maxListNumber = 10;
 
 
@@ -43,9 +42,64 @@ public sealed class PoolManager
             { EffectType.SmokeEffect, new List<GameObject>() },
             { EffectType.UnCoveredEffect, new List<GameObject>() },
             { EffectType.GoldEffect, new List<GameObject>() },
-            { EffectType.DoorOopenEffect, new List<GameObject>() }
         };
         effectGOPrefabDic = new Dictionary<EffectType, GameObject>();
         return this;
+    }
+
+    public GameObject GetInstance(EffectType _type, Transform parent = null, bool isWorldPos = false)
+    {
+        lock (effectPoolDic[_type])
+        {
+            var list = effectPoolDic[_type];
+            if (list.Count > 0)
+            {
+                GameObject tempGO = list[list.Count - 1];
+                tempGO.SetActive(true);
+                ResetInstance(tempGO);
+                tempGO.transform.SetParent(parent, isWorldPos);
+                list.Remove(tempGO);
+                return tempGO;
+            }
+            else
+            {
+                var tempGO = GameObject.Instantiate(effectGOPrefabDic[_type], parent, isWorldPos);
+                return tempGO;
+            }
+        }
+    }
+
+    public void ResetInstance(GameObject go)
+    {
+        ParticleSystem ps = go.GetComponent<ParticleSystem>();
+        if (ps != null)
+        {
+            ps.Stop();
+            ps.Play();
+        }
+
+        foreach (Transform t in go.transform)
+        {
+            ParticleSystem tempPS = t.GetComponent<ParticleSystem>();
+            if (tempPS != null)
+            {
+                tempPS.Stop();
+                tempPS.Play();
+            }
+        }
+    }
+
+    public void StoreInstance(EffectType _type,GameObject go)
+    {
+        List<GameObject> list= effectPoolDic[_type];
+        if(list.Count<maxListNumber)
+        {
+            go.SetActive(false);
+            list.Add(go);
+        }
+        else
+        {
+            GameObject.Destroy(go);
+        }
     }
 }
